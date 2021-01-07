a985ace10d9cdc6d139baff3b14cc0a7c34edb04
Pawan Maran <49287277+Pavanmaran@users.noreply.github.com>
2021-01-07 15:45:49 +0530

    Create Main_activity.java

---
+++ Main_activity.java
@@ -0,0 +1,50 @@
+package com.example.project_valley_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        }
+    private void callInstagram(View view) {
+        String apppackage = "com.instagram.android";
+        Context cx=this;
+        try {
+            Intent i = cx.getPackageManager().getLaunchIntentForPackage(apppackage);
+            cx.startActivity(i);
+        } catch (Exception  e) {
+            Toast.makeText(this, "Sorry, Instagram Apps Not Found", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
